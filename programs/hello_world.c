1fcd943a9e3aea734220f7267d09611c9630bc3c
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-03 18:19:33 +0700

    feat: add Hello World (C)

---
+++ programs/hello_world.c
@@ -0,0 +1,12 @@
+/*
+ * hello_world.c
+ * Program pertama dalam bahasa C.
+ * Kompilasi: gcc hello_world.c -o hello_world && ./hello_world
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, World!\n");
+    return 0;
+}
