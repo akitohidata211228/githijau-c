a810045ab6e79f829c619948e4c0337aabd5dec5
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-06 17:47:23 +0700

    feat: add Tabel Perkalian 18 (C)

---
+++ programs/multiplication_table_of_18.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_18.c
+ * Tabel perkalian 18. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("18 x %d = %d\n", i, 18 * i);
+    }
+    return 0;
+}
