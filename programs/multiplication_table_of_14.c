3cc2c644d190d107b4a7436a4ae45997fe0fdf97
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-06 12:37:20 +0700

    feat: add Tabel Perkalian 14 (C)

---
+++ programs/multiplication_table_of_14.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_14.c
+ * Tabel perkalian 14. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("14 x %d = %d\n", i, 14 * i);
+    }
+    return 0;
+}
