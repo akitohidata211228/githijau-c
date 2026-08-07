16dde601d215b172b7b361df45744f90d3b98401
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-07 09:40:33 +0700

    feat: add Tabel Perkalian 22 (C)

---
+++ programs/multiplication_table_of_22.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_22.c
+ * Tabel perkalian 22. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("22 x %d = %d\n", i, 22 * i);
+    }
+    return 0;
+}
