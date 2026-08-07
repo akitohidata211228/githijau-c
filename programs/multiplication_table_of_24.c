aaf6fdb40c3a71c7d2b9edfe007fecd6c597da9d
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-07 13:15:42 +0700

    feat: add Tabel Perkalian 24 (C)

---
+++ programs/multiplication_table_of_24.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_24.c
+ * Tabel perkalian 24. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("24 x %d = %d\n", i, 24 * i);
+    }
+    return 0;
+}
