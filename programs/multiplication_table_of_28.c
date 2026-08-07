c5c17bd84d759e218220cd9cc5464712bc5ffff4
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-07 19:54:12 +0700

    feat: add Tabel Perkalian 28 (C)

---
+++ programs/multiplication_table_of_28.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_28.c
+ * Tabel perkalian 28. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("28 x %d = %d\n", i, 28 * i);
+    }
+    return 0;
+}
