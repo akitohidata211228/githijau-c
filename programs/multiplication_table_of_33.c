87b375a1d99a769148e4b5b8192d8c5cd910184c
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-08 20:24:21 +0700

    feat: add Tabel Perkalian 33 (C)

---
+++ programs/multiplication_table_of_33.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_33.c
+ * Tabel perkalian 33. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("33 x %d = %d\n", i, 33 * i);
+    }
+    return 0;
+}
