d5148cc9a62ebcf9a1c51a78c7015fbe1a21bbfa
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-08 20:48:21 +0700

    feat: add Tabel Perkalian 35 (C)

---
+++ programs/multiplication_table_of_35.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_35.c
+ * Tabel perkalian 35. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("35 x %d = %d\n", i, 35 * i);
+    }
+    return 0;
+}
