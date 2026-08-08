b598fdec704fff6c53e743b1ab71d8fead86ce2f
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-08 21:00:22 +0700

    feat: add Tabel Perkalian 36 (C)

---
+++ programs/multiplication_table_of_36.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_36.c
+ * Tabel perkalian 36. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("36 x %d = %d\n", i, 36 * i);
+    }
+    return 0;
+}
