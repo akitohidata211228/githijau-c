7d14874cd13572d95bf09d55277c26812416f076
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-12 14:08:03 +0700

    feat: add Tabel Perkalian 40 (C)

---
+++ programs/multiplication_table_of_40.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_40.c
+ * Tabel perkalian 40. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("40 x %d = %d\n", i, 40 * i);
+    }
+    return 0;
+}
