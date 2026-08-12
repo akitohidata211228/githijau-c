3647cddb9b6bb45cbde7ca05899716956a07d93f
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-12 13:42:03 +0700

    feat: add Tabel Perkalian 38 (C)

---
+++ programs/multiplication_table_of_38.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_38.c
+ * Tabel perkalian 38. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("38 x %d = %d\n", i, 38 * i);
+    }
+    return 0;
+}
