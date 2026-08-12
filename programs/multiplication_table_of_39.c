854091b7c06ec31dda948e3dc8a52e436517e628
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-12 13:55:03 +0700

    feat: add Tabel Perkalian 39 (C)

---
+++ programs/multiplication_table_of_39.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_39.c
+ * Tabel perkalian 39. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("39 x %d = %d\n", i, 39 * i);
+    }
+    return 0;
+}
