4d4735198039a8da28cd443505f68e3246d6846c
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-13 14:26:25 +0700

    feat: add Tabel Perkalian 47 (C)

---
+++ programs/multiplication_table_of_47.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_47.c
+ * Tabel perkalian 47. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("47 x %d = %d\n", i, 47 * i);
+    }
+    return 0;
+}
