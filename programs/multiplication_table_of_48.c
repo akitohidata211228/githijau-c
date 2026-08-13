50e7f25365ce0745105eb85392ee86c332d74aac
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-13 16:29:25 +0700

    feat: add Tabel Perkalian 48 (C)

---
+++ programs/multiplication_table_of_48.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_48.c
+ * Tabel perkalian 48. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("48 x %d = %d\n", i, 48 * i);
+    }
+    return 0;
+}
