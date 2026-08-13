7111bf008441c181c82eb47584a1782d8ae7e9e3
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-13 14:02:26 +0700

    feat: add Tabel Perkalian 46 (C)

---
+++ programs/multiplication_table_of_46.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_46.c
+ * Tabel perkalian 46. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("46 x %d = %d\n", i, 46 * i);
+    }
+    return 0;
+}
