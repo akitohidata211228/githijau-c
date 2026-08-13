b4fc2603fd384473568a6e2b1f04aa0663701386
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-13 22:23:56 +0700

    feat: add Tabel Perkalian 51 (C)

---
+++ programs/multiplication_table_of_51.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_51.c
+ * Tabel perkalian 51. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("51 x %d = %d\n", i, 51 * i);
+    }
+    return 0;
+}
