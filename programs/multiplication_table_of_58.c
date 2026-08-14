4f38b68fee60155352363a9d03368cd6edaba159
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-14 20:47:09 +0700

    feat: add Tabel Perkalian 58 (C)

---
+++ programs/multiplication_table_of_58.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_58.c
+ * Tabel perkalian 58. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("58 x %d = %d\n", i, 58 * i);
+    }
+    return 0;
+}
