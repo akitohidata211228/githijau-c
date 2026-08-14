2caa3b449133fed94fcb36b637de6346bb95afcd
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-14 12:48:10 +0700

    feat: add Tabel Perkalian 54 (C)

---
+++ programs/multiplication_table_of_54.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_54.c
+ * Tabel perkalian 54. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("54 x %d = %d\n", i, 54 * i);
+    }
+    return 0;
+}
