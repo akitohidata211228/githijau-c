123c08359c497c8c42a2549c234660fa86ea559a
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-15 22:17:19 +0700

    feat: add Tabel Perkalian 66 (C)

---
+++ programs/multiplication_table_of_66.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_66.c
+ * Tabel perkalian 66. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("66 x %d = %d\n", i, 66 * i);
+    }
+    return 0;
+}
