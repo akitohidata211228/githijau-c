5c7ee52bd1eec76054d71b3c4147ab31974d87cd
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-15 16:04:24 +0700

    feat: add Tabel Perkalian 63 (C)

---
+++ programs/multiplication_table_of_63.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_63.c
+ * Tabel perkalian 63. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("63 x %d = %d\n", i, 63 * i);
+    }
+    return 0;
+}
