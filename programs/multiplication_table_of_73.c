96b0e97babbb6f7c6371b2707710e927a80a7e1a
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-16 19:56:43 +0700

    feat: add Tabel Perkalian 73 (C)

---
+++ programs/multiplication_table_of_73.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_73.c
+ * Tabel perkalian 73. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("73 x %d = %d\n", i, 73 * i);
+    }
+    return 0;
+}
