85502e130997963c182a582637334f87abef6bb8
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-17 17:51:47 +0700

    feat: add Tabel Perkalian 79 (C)

---
+++ programs/multiplication_table_of_79.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_79.c
+ * Tabel perkalian 79. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("79 x %d = %d\n", i, 79 * i);
+    }
+    return 0;
+}
