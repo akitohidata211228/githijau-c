fec304a21eb6c5054625a2daa77a3289ea04cbec
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-17 22:45:50 +0700

    feat: add Tabel Perkalian 82 (C)

---
+++ programs/multiplication_table_of_82.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_82.c
+ * Tabel perkalian 82. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("82 x %d = %d\n", i, 82 * i);
+    }
+    return 0;
+}
