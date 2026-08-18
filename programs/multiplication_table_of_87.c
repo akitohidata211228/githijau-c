49e7dfb003e4cae5692528cd9791e96b99f7261e
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-18 17:11:29 +0700

    feat: add Tabel Perkalian 87 (C)

---
+++ programs/multiplication_table_of_87.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_87.c
+ * Tabel perkalian 87. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("87 x %d = %d\n", i, 87 * i);
+    }
+    return 0;
+}
