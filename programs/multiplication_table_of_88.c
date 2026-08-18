f1d114946e448587a0556a19cd0a8a14d0a22761
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-18 19:13:29 +0700

    feat: add Tabel Perkalian 88 (C)

---
+++ programs/multiplication_table_of_88.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_88.c
+ * Tabel perkalian 88. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("88 x %d = %d\n", i, 88 * i);
+    }
+    return 0;
+}
