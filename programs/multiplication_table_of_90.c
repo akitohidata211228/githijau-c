5f5bbe40ec1909b54f602fddebd6acbc336a221a
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-19 08:39:01 +0700

    feat: add Tabel Perkalian 90 (C)

---
+++ programs/multiplication_table_of_90.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_90.c
+ * Tabel perkalian 90. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("90 x %d = %d\n", i, 90 * i);
+    }
+    return 0;
+}
