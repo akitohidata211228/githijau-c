4361a8961d8fe42dac42e886855c8f13ef5e7cf5
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-19 18:14:26 +0700

    feat: add Tabel Perkalian 94 (C)

---
+++ programs/multiplication_table_of_94.c
@@ -0,0 +1,11 @@
+/* multiplication_table_of_94.c
+ * Tabel perkalian 94. */
+
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 10; i++) {
+        printf("94 x %d = %d\n", i, 94 * i);
+    }
+    return 0;
+}
